f88452558633ec704d635e15aa27470a0cc935bf
andrea-livornese <andrealivornese@gmail.com>
2024-02-05 16:22:31 +0100

    Snack 1: fine struttura dello script

--- snack 1/script.js
+++ snack 1/script.js
@@ -1,3 +1,14 @@
 // Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50
 
 
+const array=[];
+
+
+let somma=0;
+
+do{
+    somma+= Number(prompt("inserisci un numero"));
+    console.log(somma);
+}while(somma < 50);
+
+console.log("fuori dal while");
